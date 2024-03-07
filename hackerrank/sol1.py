3b486b37d32e275b753d0afc5f48a2de2f86b3e5
nagmat <nagmat@gmail.com>
2024-03-07 13:39:22 -0600

    Create sol1.py

---
+++ hackerrank/sol1.py
@@ -0,0 +1,31 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    b = [x for x in a if a.count(x)==1]
+    return b[0]
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
